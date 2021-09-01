9bd6dcfe05a2ba02d9cfa59ec92ffbad4f6486d4
john million <jpmillion@comcast.net>
2021-09-01 16:08:51 -0400

    Define factorial and isPrime algorithms and tests in java

--- src/math/MathAlgo.java
+++ src/math/MathAlgo.java
@@ -2,6 +2,10 @@ package math;
 
 public class MathAlgo {
 	public static int fib(int n) {
+		if (n < 1) {
+			throw new IllegalArgumentException("Input should be positive");
+		}
+		
 		int nMinusTwo = 0;
 		int nMinusOne = 1;
 		
@@ -13,4 +17,32 @@ public class MathAlgo {
 		
 		return nMinusOne + nMinusTwo;
 	}
+	
+	public static long fac(long n) {
+		
+		if (n < 0) {
+			throw new IllegalArgumentException("Input should be positive");
+		}
+		
+		long product = 1;
+		
+		for (long i = n; i > 1; i--) {
+			product *= i;
+		}
+		
+		return product;
+	}
+	
+	public static boolean isPrime(int n) {
+		if (n < 2) return false;
+		
+		int squareRoot = (int) Math.sqrt((double) n);
+		
+		for (int i = 3; i <= squareRoot; i += 2) {
+			if (n % i == 0) return false;
+		}
+		
+		return true;
+	}
+	
 }
--- src/testMath/MathAlgoTest.java
+++ src/testMath/MathAlgoTest.java
@@ -13,6 +13,29 @@ public class MathAlgoTest {
 		assertEquals("should equal 13", 13, MathAlgo.fib(7));
 		assertEquals("should equal 1", 1, MathAlgo.fib(1));
 		assertEquals("should equal 1", 1, MathAlgo.fib(2));
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			MathAlgo.fib(-2);
+		});
+	}
+	
+	@Test
+	public void testFac() {
+		assertEquals("should equal 120", 120, MathAlgo.fac(5));
+		assertEquals("should equal 720", 720, MathAlgo.fac(6));
+		assertEquals("should equal 1", 1, MathAlgo.fac(1));
+		assertEquals("should equal 1", 1, MathAlgo.fac(0));
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			MathAlgo.fac(-2);
+		});
 	}
 
+	@Test
+	public void testIsPrime() {
+		assertEquals("should be true", true, MathAlgo.isPrime(13));
+		assertEquals("should be false", false, MathAlgo.isPrime(25));
+		assertEquals("should be false", false, MathAlgo.isPrime(0));
+		assertEquals("should be true", true, MathAlgo.isPrime(23));
+	}
 }
